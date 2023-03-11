d6aa54d5d56bc9ea3582762eb7a0d52a1f19969d
Ehsan138 <67409692+Ehsan138@users.noreply.github.com>
2023-03-11 11:57:09 -0800

    initial comment

---
+++ SpeechToText.js
@@ -0,0 +1,42 @@
+const startBtn = document.getElementById('start-btn');
+const stopBtn = document.getElementById('stop-btn');
+const transcriptionDiv = document.getElementById('transcription');
+const responseDiv = document.getElementById('response');
+let response = '';
+const recognition = new webkitSpeechRecognition();
+recognition.continuous = true;
+recognition.interimResults = true;
+recognition.lang = 'en-US';
+startBtn.addEventListener('click', function() {
+  response = '';
+  recognition.start();
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+});
+stopBtn.addEventListener('click', function() {
+  recognition.stop();
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+  responseDiv.textContent = response;
+});
+recognition.onresult = function(event) {
+  let finalTranscription = '';
+  let interimTranscription = '';
+  for (let i = event.resultIndex; i < event.results.length; ++i) {
+    const result = event.results[i];
+    if (result.isFinal) {
+      const transcript = result[0].transcript.trim();
+      if (response.indexOf(transcript) === -1) {
+        response += transcript + ' ';
+      }
+      finalTranscription += transcript;
+    } else {
+      interimTranscription += result[0].transcript;
+    }
+  }
+  transcriptionDiv.textContent = finalTranscription + interimTranscription;
+  console.log(response);
+};
+recognition.onerror = function(event) {
+  console.error('Recognition error: ', event.error);
+};
