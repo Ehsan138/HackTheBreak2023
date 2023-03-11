8434e956d32e03e08e578f69d2c1c1f2c3ed9a17
CadanGlass <113327002+CadanGlass@users.noreply.github.com>
2023-03-11 14:50:54 -0800

    Updated dark mode icon to switch and change text

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 function darkMode() {
-    var main = document.querySelector("main");
+     let icon = document.querySelector(".mode-icon");
+     let linkText = document.querySelector(".link-text")
+
+     if (icon.textContent === "🌘") {
+       icon.textContent = "☀️";
+        linkText.textContent = "Light Mode"
+     } else {
+       icon.textContent = "🌘";
+       linkText.textContent = "Dark Mode";
+     }
+
+    let main = document.querySelector("main");
     main.classList.toggle("dark-mode");
     console.log("clicked")
 }
