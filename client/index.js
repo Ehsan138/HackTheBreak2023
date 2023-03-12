6bc8aa44159cc3ddd626390d7c113c8a28e6560c
ankitahlwat <ankitahlwat54@gmail.com>
2023-03-11 16:12:52 -0800

    New and improved

--- client/index.js
+++ client/index.js
@@ -1,16 +1,31 @@
+
+
+$(document).ready(function () {
+  $('#speech').hide(); // hide the textbox
+
+  $('.record-button').click(function () {
+    $('#ai-ouput').hide(); // hide the textbox
+    $('form').hide(); // hide the textbox
+    $('#ai-ouput').prop('disabled', true); // disable the textbox
+    $('.output-textbox label').text('Job description submitted.'); // update the label text
+    $('#speech').show(); // hide the textbox
+
+  });
+})
+
 function darkMode() {
-     let icon = document.querySelector(".mode-icon");
-     let linkText = document.querySelector(".link-text")
+  let icon = document.querySelector(".mode-icon");
+  let linkText = document.querySelector(".link-text")
 
-     if (icon.textContent === "🌘") {
-       icon.textContent = "☀️";
-        linkText.textContent = "Light Mode"
-     } else {
-       icon.textContent = "🌘";
-       linkText.textContent = "Dark Mode";
-     }
+  if (icon.textContent === "🌘") {
+    icon.textContent = "☀️";
+    linkText.textContent = "Light Mode"
+  } else {
+    icon.textContent = "🌘";
+    linkText.textContent = "Dark Mode";
+  }
 
-    let main = document.querySelector("main");
-    main.classList.toggle("dark-mode");
-    console.log("clicked")
+  let main = document.querySelector("main");
+  main.classList.toggle("dark-mode");
+  console.log("clicked")
 }
